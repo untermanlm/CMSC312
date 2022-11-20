e34d4771c70f14ae8be0b5f2c6e90775d26d299a
untermanlm <80475070+untermanlm@users.noreply.github.com>
2022-11-20 17:41:45 -0500

    Update Operations.py

--- src/Operations.py
+++ src/Operations.py
@@ -1,21 +1,22 @@
 import random
 import time
-import multiprocessing
-
+import threading
 
 def shortestJobFirst(process):
     operationList = process.operationsList
     sorted_1 = sorted(operationList, key=lambda x: x[1])
     for operation in sorted_1:
+        if(process.stop):
+            break
         operator = operation[0]
         cycles = operation[1]
         if (operator == 'CALCULATE'):
             process._state = 'RUN'
-            print(process.state)
+            print(f'Process {process.id}: {process.state}')
             calculate(cycles)
         elif (operator == 'I/O'):
             process._state = 'WAIT'
-            print(process.state)
+            print(f'Process {process.id}: {process.state}')
             io(cycles)
         elif (operator == 'FORK'):
             print()
@@ -24,22 +25,24 @@ def shortestJobFirst(process):
 def firstComeFirstServe(process):
     operationList = process.operationsList
     for operation in operationList:
+        if (process.stop):
+            break
         operator = operation[0]
         cycles = operation[1]
         if (operator == 'CALCULATE'):
             process._state = 'RUN'
-            print(process.state)
+            print(f'Process {process.id}: {process.state}')
             calculate(cycles)
         elif (operator == 'I/O'):
             process._state = 'WAIT'
-            print(process.state)
+            print(f'Process {process.id}: {process.state}')
             io(cycles)
         elif (operator == 'FORK'):
             print()
         else:
             print("Unknown operator")
 def calculate(numCycles):
-    lock = multiprocessing.Lock()
+    lock = threading.Lock()
     temp = numCycles
     calculation = 0
     while(numCycles > 0):
@@ -47,17 +50,17 @@ def calculate(numCycles):
             numCycles -= 1
             calculation += 1 #arbitrary calculation
             time.sleep(0.05)
-            print(f"Calculating: {numCycles} / {temp} cycles remaining.\n")
-    print()
+            #print(f"Calculating: {numCycles} / {temp} cycles remaining.\n")
+    #print()
 def io(numCycles):
-    lock = multiprocessing.Lock()
+    lock = threading.Lock()
     temp = numCycles
     while (numCycles > 0):
         with lock:
             numCycles -= 1
             time.sleep(0.05)
-            print(f"Waiting: {numCycles} / {temp} cycles remaining.\n")
-    print()
+            #print(f"Waiting: {numCycles} / {temp} cycles remaining.\n")
+    #print()
 
 #to be developed
 def fork():
