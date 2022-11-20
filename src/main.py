573dbb22a394bd8b99399138f4e3e13031b5aa32
untermanlm <80475070+untermanlm@users.noreply.github.com>
2022-11-20 17:42:17 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,24 +1,20 @@
-import multiprocessing
-import sys
-from datetime import time
-from time import sleep
-
+import threading
 from SeedGenerator import readProgramFile, createProcess
 import Process
-import psutil
 import tkinter as tk
-import subprocess as sub
 validOperations = ['CALCULATE', 'I/O', 'FORK']
 # def update():
 #     text = tk.Text(root)
-#     curr_process = multiprocessing.current_process()
-#     curr_process.data
-#     #str1 = 'ID: ' + str(curr_process.id) + ' STATE: ' + curr_process.state + '\n'
-#     text.insert(tk.INSERT, str1)
-#     text.pack()
+#     stats = []
+#     for p in processes:
+#         stat = 'ID: ' + str(p.id) + ' STATE: ' + p.state + '\n'
+#         stats.append(stat)
+#     for stat in stats:
+#         text.insert(tk.INSERT, stat)
+#         text.pack()
 #     root.after(1000, update)
 def callback(*args):
-    global processList
+    global operationsList
     global instructions
     msg = e.get()
     if ('LOAD' in msg):
@@ -42,25 +38,31 @@ def callback(*args):
                 if (i == numProcesses - 1):
                     p.scheduleType = 'SJF'
                 processes.append(p)
+                operationsList.append(processList)
         except ValueError:
             print('Invalid number of processes.')
+        except UnboundLocalError:
+            print('Need to load processes first!')
     if(msg == 'START'):
-        print(processList)
+        print(operationsList)
         print(processes)
         for p in processes:
-            p.start()
+            if (p.state == 'READY'):
+                p.start()
+            else:
+                while(Process.m1.getMemoryLeft() < p.memoryreq):
+                    p.wait()
+                Process.m1.useMemory(p.memoryreq, p)
+                p.start()
         for p in processes:
-            p.state = 'EXIT'
             p.join()
     if (msg == 'STATS'):
         print('Printing stats!')
         for p in processes:
-            print(f'ID: {p.id}, State: {p.state}, Schedule Type: {p.scheduleType}')
+            print(f'ID: {p.id}, State: {p.state}, Schedule Type: {p.scheduleType}, Memory Requirement: {p.memoryreq} MB')
         print("Individual log files will be stored as processLog(id).txt")
         # sys.stdout.close()
         # sys.stdout = stdoutOrigin
-
-
     if('SUSPEND' in msg):
         try:
             numCycles = int(msg[8:])
@@ -68,27 +70,23 @@ def callback(*args):
             for p in processes:
                 p.start()
             for p in processes:
-                #p.state = 'EXIT'
+                p.stop = True
                 p.join(0.08 * numCycles)
                 if p.is_alive():
-                    p.terminate()
                     p.join()
-
         except ValueError:
             print('Invalid number of cycles.')
     if(msg == 'CLEAR'):
         print('Clearing processes')
-        processList.clear()
+        operationsList.clear()
         processes.clear()
-
-
 def main():
     print()
 if __name__ == "__main__":
     # stdoutOrigin = sys.stdout
     # sys.stdout = open('processLog.txt', 'w')
     instructions = []
-    processList = []
+    operationsList = []
     processes = []
 
     root = tk.Tk()
@@ -107,5 +105,3 @@ if __name__ == "__main__":
 
     #update()
     root.mainloop()
-
-
